ba2d09ccd60d677c00cf5feb67b4a224af8f26dd
Rustam Guseinov <rg20031991.com>
2023-05-11 20:48:18 +0300

    get an id using routing

--- src/pages/contact/[id].tsx
+++ src/pages/contact/[id].tsx
@@ -2,12 +2,15 @@ import {useRouter} from 'next/router';
 
 export default function Contactİd() {
     const router = useRouter();
+    //создаем экземпляр useRouter который предоставляет данные о параметрах данного маршрута  
     const {id} = router.query;
+    //извлекаем id из объекта query, расположенного в объекте в экземпляре  useRouter
 
   return (
     <div>
         <p>
-            At a specific contacts
+            At ID number: {id}
+            {/* отображаем полученный ID */}
         </p>
         </div>
   )
